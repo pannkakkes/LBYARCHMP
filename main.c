59636224611ef1fe8f135d73dc7e17d41092c957
pannkakkes <65591903+pannkakkes@users.noreply.github.com>
2024-12-01 15:02:14 +0800

    Add function headers

--- main.c
+++ main.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <windows.h>
 
-extern void asmhello();
+void c_kernel_dot_product(int n, double* vectorA, double* vectorB, double* sdot);
+extern void asmhello(int n, double* vectorA, double* vectorB, double* sdot);
 
 int main() {
+	int n = 4;
+	double vectorA[] = { 1.0, 2.0, 3.0, 4.0 };
+	double vectorB[] = { 5.0, 6.0, 7.0, 8.0 };
+	double sdot;
+
 	asmhello();
 	return 0;
 }
